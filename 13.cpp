1e0ad23a5cf39d6730331aec93cd2721cb6c5814
gx_wind <wind.guoxing@gmail.com>
2017-08-29 21:05:00 +0800

    add problem 13

---
+++ 13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int strStr(const char *source, const char *target) {
+    // write your code here
+    int m = strlen(source), n = strlen(target);
+    for (int i = 0; i < m-n+1; i++) {
+        int j;
+        for (j = 0; j < n; j++) {
+            if (source[i+j] != target[j])
+                break;
+        }
+        if (j == n) 
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    return 0;
+}
